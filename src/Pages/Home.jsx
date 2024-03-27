1b85dfe3fbe4e436d491b4a375391bb4e0a59f8c
Abdumannof <160102960+Abduval1ev@users.noreply.github.com>
2024-03-27 17:18:45 +0300

    #fix #scss #js FoodItem larga Count holat berildi va malumotlar Count hisobida kopayyapti.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header/Header'
 import ExploreMenu from '../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../components/FoodDisplay/FoodDisplay'
 
 export default function Home() {
 
-  const [category, setCategory] = React.useState('All')
+  const [category, setCategory] = useState('All')
 
   return (
     <>
@@ -14,7 +14,7 @@ export default function Home() {
         category={category}
         setCategory={setCategory}
       />
-      <FoodDisplay />
+      <FoodDisplay category={category} />
     </>
   )
 }
--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-export default function FoodDisplay() {
+export default function FoodDisplay({ category }) {
 
-    const { food_list } = React.useContext(StoreContext)
+    const { food_list } = useContext(StoreContext)
 
     return (
         <>
             <div className="food_display" id="food_display">
                 <h2 className='food_display_heading'>Top dishes near you</h2>
                 <div className="food_dispaly_list">
-                    {food_list.map((item, index) => {
-                        return <FoodItem
-                            key={index}
-                            id={item.id}
-                            name={item.name}
-                            price={item.price}
-                            image={item.image}
-                            description={item.description}
-                        />
-                    })}
+                    {food_list.leight ? (food_list.map((item, index) => {
+                        if (category === "All" || category === item.category) {
+                            return <FoodItem
+                                key={index}
+                                id={item.id}
+                                name={item.name}
+                                price={item.price}
+                                image={item.image}
+                                description={item.description}
+                            />
+                        }
+                    })) : <p>No Menu!!</p>}
                 </div>
             </div>
         </>
