5576d36f2019c06f85ef9fe6c61059871a8e15de
Abdumannof <160102960+Abduval1ev@users.noreply.github.com>
2024-04-01 16:15:31 +0300

    #fix #scss #js Sayitning funksional va vorstka qismi tugatildi va i18n berish boshlandi.

--- src/Pages/PlaceOrder.jsx
+++ src/Pages/PlaceOrder.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { StoreContext } from '../components/context/StoreContext'
 
 export default function PlaceOrder() {
-  return (
-    <>
 
-    </>
+  const { getTotalCartAmount } = React.useContext(StoreContext)
+
+  return (
+    <form className='plase_order'>
+      <div className="place_order_left">
+        <p className="title">Delivery Information</p>
+        <div className="multi_fields">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="email" placeholder='Email addres' />
+        <input type="text" placeholder='Store' />
+        <div className="multi_fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi_fields">
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+      <div className="place_order_right">
+        <div className="cart_total">
+          <h2 className='place_order_right_title'>Cart Totals</h2>
+          <div>
+            <div className="cart_total_details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr className='cart_total_hr' />
+            <div className="cart_total_details">
+              <p>Deliver Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr className='cart_total_hr' />
+            <div className="cart_total_details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button className='cart_total_btn'>PROCEED TO PAYMET</button>
+        </div>
+      </div>
+    </form >
   )
 }
